6a90ef06437a3e1067b3c7c6aeae7711325b6b2b
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2025-03-10 12:06:37 +0300

    fix: remove model initiation in loop

--- scripts/create_embeddings_dataset.py
+++ scripts/create_embeddings_dataset.py
@@ -5,7 +5,8 @@ import pandas as pd
 from embeddings.embedder import ImageBindEmbedder
 
 
-def create_images_dataset(images_dir: str, output_path: str = "../data/images_dataset.csv", count: int = 10) -> pd.DataFrame:
+def create_images_dataset(images_dir: str, output_path: str = "../data/images_dataset.csv",
+                          count: int = 10) -> pd.DataFrame:
     image_files = os.listdir(images_dir)
     image_files = [f for f in image_files if f.endswith(".jpg")]
 
@@ -30,24 +31,25 @@ def batch_create_images_dataset(images_dir: str, output_dir: str, batch_size: in
     image_files = os.listdir(images_dir)
     image_files = [f for f in image_files if f.endswith(".jpg")]
     embedded_count = 0
-    for i in range(0, len(image_files), batch_size):
+    image_paths = [os.path.join(images_dir, f) for f in image_files][:count]
+    embeddings = []
+    print(f"Number of images: {len(image_files)}")
+    embedder = ImageBindEmbedder()
+    output_path = os.path.join(output_dir, f"images_dataset.pkl")
+    for i in range(0, min(len(image_files), count), batch_size):
         if embedded_count >= count:
             break
         batch_image_files = image_files[i:i + batch_size]
         batch_image_paths = [os.path.join(images_dir, f) for f in batch_image_files]
-        print(f"Number of images: {len(batch_image_files)}")
         for j, f in enumerate(batch_image_files):
             print(f"{j}: {f}")
 
-        df = pd.DataFrame(batch_image_files, columns=["image_path"], index=range(len(batch_image_files)))
+        batch_embeddings = embedder.embed_image(batch_image_paths)
+        embeddings.extend(batch_embeddings)
 
-        embedder = ImageBindEmbedder()
-        embeddings = embedder.embed_image(batch_image_paths)
-        df["embeddings"] = embeddings.tolist()
-
-        output_path = os.path.join(output_dir, f"images_dataset_{i}.csv")
-        df.to_csv(output_path, index=False)
-        embedded_count += len(batch_image_files)
+    df = pd.DataFrame({"image_path": image_paths[:count], "embeddings": embeddings}, columns=["image_path", "embeddings"],
+                      index=range(count))
+    df.to_pickle(output_path)
 
 
 def create_audio_dataset(audio_dir: str, count: int = 10) -> pd.DataFrame:
