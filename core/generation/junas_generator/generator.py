9c32b2283b9b7f830ca68e26f7db0809e522ee79
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2025-04-06 16:39:37 +0300

    feat: concat audio embeds to LlamaModel's input embeddings

--- core/generation/junas_generator/generator.py
+++ core/generation/junas_generator/generator.py
@@ -1,4 +1,3 @@
-import logging
 from abc import ABC
 
 import numpy as np
@@ -8,8 +7,11 @@ from PIL import Image
 from transformers import AutoModelForCausalLM
 from janus.models import MultiModalityCausalLM, VLChatProcessor
 from core.generation.__base import BaseImageGenerator
+from utils import logs
 
 AVAILABLE_MODELS = ["Janus-1.3B", "Janus-Pro-7B", "JanusFlow-1.3B"]
+
+logger = logs.create_logger(__name__.split(".")[-1])
 
 
 class JanusImageGenerator(BaseImageGenerator, ABC):
@@ -18,8 +20,9 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
     @classmethod
     def init_model(cls, config):
         model_name = config.get("model_name", "Janus-1.3B")
+        cls.sys_prompt = config.get("sys_prompt", "Abstract art for representing emotions")
         if model_name not in AVAILABLE_MODELS:
-            logging.warning(f"Model {model_name} not available. Using {AVAILABLE_MODELS[0]} instead.")
+            logger.warning(f"Model {model_name} not available. Using {AVAILABLE_MODELS[0]} instead.")
             model_name = AVAILABLE_MODELS[0]
 
         model_path = f"deepseek-ai/{model_name}"
@@ -31,16 +34,13 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
         )
         cls.vl_gpt = cls.vl_gpt.to(torch.bfloat16).cuda().eval()
 
-        def test_model(inputs):
-            return np.random.rand(3, 224, 224)
-
-        cls.model = test_model
-
+        cls.model = cls.vl_gpt
+        cls.audio_embeds_shape = {0: 2, 2: 2048}
+        cls.audio_embeds_type = torch.bfloat16
         # hardcoded need to learn more about this
         cls.parallel_size = 1
         cls.img_size = 384
-        cls. patch_size = 16
-
+        cls.patch_size = 16
 
     @classmethod
     def _preprocess_input(cls, inputs):
@@ -62,9 +62,9 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
 
     @classmethod
     def _postprocess_output(cls, outputs):
+        concated = torch.cat([outputs, torch.zeros_like(outputs)], dim=1)
         outputs = outputs.numpy()
         outputs = np.clip((outputs + 1) / 2 * 255, 0, 255)
-
         image = Image.fromarray(outputs.astype(np.uint8))
 
         return image
@@ -74,6 +74,7 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
                      temperature: float = 1,
                      cfg_weight: float = 5,
                      image_token_num_per_image: int = 576,
+                     audio_embeds: torch.Tensor = None,
                      **kwargs):
 
         input_ids = cls.vl_chat_processor.tokenizer.encode(prompt)
@@ -85,7 +86,10 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
             if i % 2 != 0:
                 tokens[i, 1:-1] = cls.vl_chat_processor.pad_id
         cls.vl_gpt.language_model.config._attn_implementation = 'eager'
+
         inputs_embeds = cls.vl_gpt.language_model.get_input_embeddings()(tokens)
+        if audio_embeds:
+            inputs_embeds = torch.cat([audio_embeds, inputs_embeds], dim=1)
 
         generated_tokens = torch.zeros((cls.parallel_size, image_token_num_per_image), dtype=torch.int).cuda()
 
@@ -109,7 +113,8 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
             inputs_embeds = img_embeds.unsqueeze(dim=1)
 
         dec: torch.Tensor = cls.vl_gpt.gen_vision_model.decode_code(generated_tokens.to(dtype=torch.int),
-                                                                    shape=[cls.parallel_size, 8, cls.img_size // cls.patch_size,
+                                                                    shape=[cls.parallel_size, 8,
+                                                                           cls.img_size // cls.patch_size,
                                                                            cls.img_size // cls.patch_size])
         dec = dec.to(torch.float32).cpu().permute(0, 2, 3, 1)[0]
 
@@ -117,12 +122,12 @@ class JanusImageGenerator(BaseImageGenerator, ABC):
 
     @classmethod
     def generate_from_embeds(cls, inputs: np.ndarray) -> np.ndarray:
-        input_tensor = torch.from_numpy(inputs).cuda().int()
+        prompt = cls._preprocess_input(cls.sys_prompt)
+        input_tensor = torch.from_numpy(inputs).to(cls.audio_embeds_type).cuda()
 
-        # invoke Decoder of the VQ Model to generate the image from latent
-        dec = cls.vl_gpt.gen_vision_model.decode_code(input_tensor, shape=[cls.parallel_size, 8, cls.img_size // cls.patch_size,])
+        if all([input_tensor.shape[d] == s for d, s in cls.audio_embeds_shape.items()]):
+            logger.error(f"Input tensor had shape {inputs.shape} was expected {cls.audio_embeds_shape}")
 
-        dec = dec.to(torch.float32).cpu().permute(0, 2, 3, 1)[0]
-        image = cls._postprocess_output(dec)
+        output = cls.invoke_model(prompt, audio_embeds=input_tensor)
+        image = cls._postprocess_output(output)
         return image
-
